d78e62ac7901353f99d758c08e3713d4f64d3942
abeersobhy <abeersobhy247@gmail.com>
2025-07-09 21:42:38 +0300

    Second Commit

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Factorial {
     public static void main(String[] args)
     {
-        System.out.println("Hello from Factorial");
+        Scanner in = new Scanner(System.in);
+        long num = in.nextLong();
+        if(num != 1)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
     }
 }
