bcc9238adcc8dd1683790105cf1928b01cbce111
abeersobhy <abeersobhy247@gmail.com>
2025-07-10 03:16:59 +0300

    Problem solved sucessfully

--- SearchInMatrix.java
+++ SearchInMatrix.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class SearchInMatrix {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        byte N = in.nextByte();
+        byte M = in.nextByte();
+        boolean flag = false;
+        long[][] arr;
+        arr = new long[N][M];
+
+        for(int i = 0 ; i < N; i++)
+        {
+            for (int k = 0 ; k < M; k++)
+            {
+                arr[i][k] = in.nextLong();
+            }
+        }
+        long X = in.nextLong();
+        for(int i = 0 ; i < N; i++)
+        {
+            for (int k = 0 ; k < M; k++)
+            {
+                if(X == arr[i][k])
+                {
+                    flag = true;
+                }
+            }
+        }
+        if(!flag)
+        {
+            System.out.println("will take number");
+        }
+        else
+        {
+            System.out.println("will not take number");
+        }
+    }
 }
