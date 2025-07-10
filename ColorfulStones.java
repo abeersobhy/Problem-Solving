31adf275d804689422dff89e825561bdc261f99e
abeersobhy <abeersobhy247@gmail.com>
2025-07-11 00:27:31 +0300

    New problem called ColorfulStones added

--- ColorfulStones.java
+++ ColorfulStones.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ColorfulStones {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        String StoneColor = in.nextLine();
+        String StepColor = in.nextLine();
+        byte currentPos = 0;
+
+        for(byte k = 0; k < StepColor.length(); k++)
+        {
+            if(StepColor.charAt(k) == StoneColor.charAt(currentPos))
+            {
+                if(currentPos < StoneColor.length()-1)
+                {
+                    currentPos++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        System.out.println(currentPos+1);
+    }
 }
