e832e5eef594d5b93e19af8bbf332e4609ea833f
abeersobhy <abeersobhy247@gmail.com>
2025-07-10 03:44:19 +0300

    New problem called sum of odd numbers added

--- SumOfOddNumbers.java
+++ SumOfOddNumbers.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class SumOfOddNumbers {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        byte NumOfTestCases = in.nextByte();
+        long[][] arr;
+        arr = new long[NumOfTestCases][2];
+        long[] sum;
+        sum = new long[NumOfTestCases];
+
+        for (int i = 0; i < NumOfTestCases ; i++)
+        {
+            for(int k = 0; k < 2; k++)
+            {
+                arr[i][k] = in.nextLong();
+            }
+        }
+
+        for (int i = 0; i < NumOfTestCases ; i++)
+        {
+            for(long j = (Math.min(arr[i][0], arr[i][1])) + 1 ; j < (Math.max(arr[i][0], arr[i][1])); j++)
+            {
+                if(j % 2 != 0)
+                {
+                    sum[i] += j;
+                }
+            }
+            System.out.println(sum[i]);
+        }
+    }
 }
