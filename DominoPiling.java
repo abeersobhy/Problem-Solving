7db20dcb8a9458141fd6062d6ed6167b712eef12
abeersobhy <abeersobhy247@gmail.com>
2025-07-19 13:04:57 +0300

    New solved problem Domino Pilling added

---
+++ DominoPiling.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DominoPiling
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int M = in.nextInt();
+        int N = in.nextInt();
+        System.out.println(((M*N)/2));
+    }
+}
