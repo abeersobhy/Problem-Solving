d5cf87322da8130dc3890471600f5541d0850612
abeersobhy <abeersobhy247@gmail.com>
2025-07-19 12:53:57 +0300

    New extra enhanced version of Another one bites the dust problem added

--- AnotherOneBitesTheDust.java
+++ AnotherOneBitesTheDust.java
@@ -10,29 +10,14 @@ public class AnotherOneBitesTheDust
         long numOfab = in.nextLong();
 
         long length = 0;
-
+        /*The same number of a and b*/
         if(numOfa == numOfb)
         {
             length = (2*numOfb) + (2*numOfab);
         }
         else
         {
-            if((numOfab != 0) && (numOfa != 0))
-            {
-                length = (numOfab*2) + (Math.min(numOfa , numOfb) * 2) + 1;
-            }
-            else if((numOfab != 0) && (numOfa == 0))
-            {
-                length = (numOfab*2);
-            }
-            else if((numOfab == 0) && (numOfa != 0))
-            {
-                length = (Math.min(numOfa , numOfb) * 2) + 1;
-            }
-            else
-            {
-                length = 0;
-            }
+            length = (numOfab*2) + (Math.min(numOfa , numOfb) * 2) + 1;
         }
         System.out.println(length);
     }
