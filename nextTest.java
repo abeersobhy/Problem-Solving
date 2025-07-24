6ab9951a2584fb02470ab0590ac4464acff8b733
abeersobhy <abeersobhy247@gmail.com>
2025-07-25 01:23:06 +0300

    New solved problem named nextTest added

--- nextTest.java
+++ nextTest.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class NextSession {
+public class nextTest {
 
     public static void main(String[] args)
     {
