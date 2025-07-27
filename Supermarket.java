20364033df0e9aa0d751be396e069c88500d660c
abeersobhy <abeersobhy247@gmail.com>
2025-07-27 15:47:40 +0300

    New solved problem named supermarket added

---
+++ Supermarket.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Supermarket {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int numOfSupermarkets = in.nextInt();
+        int numOfKilos = in.nextInt();
+        double[][] prices = new double[numOfSupermarkets][2];
+        double minimumPriceForKilo = 0;
+
+        for(int i = 0; i < numOfSupermarkets; i++)
+        {
+            prices[i][0] = in.nextInt(); /*Store the price.*/
+            prices[i][1] = in.nextInt(); /*Store the number of kilos for this price.*/
+        }
+
+        for(int i = 0; i < numOfSupermarkets; i++)
+        {
+            prices[i][0] = (prices[i][0] / prices[i][1]); /*Replace the price with the price for 1 kilo.*/
+        }
+
+        minimumPriceForKilo = prices[0][0];
+        for(int i = 1; i < numOfSupermarkets; i++)
+        {
+            if(minimumPriceForKilo > prices[i][0]) /*Find the minimum price for kilo.*/
+            {
+                minimumPriceForKilo = prices[i][0];
+            }
+        }
+        System.out.println(minimumPriceForKilo * numOfKilos); /*Minimum price for entered kilos.*/
+    }
+}
