aceabdd604cf25c574d4a9452a428bb3fb6dd6d2
abeersobhy <abeersobhy247@gmail.com>
2025-08-15 23:11:58 +0300

    New solved problem named Coins_2 added

---
+++ Coins_2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Coins_2
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.print(n + " ");
+
+        while (n > 1)
+        {
+            int largestDivisor = 1;
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    largestDivisor = n / i;
+                    break;
+                }
+            }
+            System.out.print(largestDivisor + " ");
+            n = largestDivisor;
+        }
+    }
+}
